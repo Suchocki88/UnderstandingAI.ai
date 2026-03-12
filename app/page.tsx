f9dbc13995059d2f530b7620f0d26c1975f0965a
Suchocki88 <John@ConceptualAcademy.com>
2026-03-12 15:29:38 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 export default function Page() {
   return (
-    <main style={{padding: "40px", fontFamily: "Arial"}}>
+    <main>
       <h1>Understanding AI</h1>
-      <p>
-        This is the first live version of the UnderstandingAI.ai website.
-      </p>
-      <p>
-        More content coming soon.
-      </p>
+      <p>This is the first live version of the UnderstandingAI.ai website.</p>
+      <p>More content coming soon.</p>
     </main>
   );
 }
